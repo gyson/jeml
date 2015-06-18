9652e5c4ea1c65fc267844d145c206009aad9c2f
gyson <eilian.yunsong@gmail.com>
2015-06-18 06:41:30 -0400

    rewrite test

--- test.js
+++ test.js
@@ -12,39 +12,48 @@ var jeml = require('./index')
 // {def x, y}
 // {def x, y, z}
 //
-test('def block', function (t) {
-    var fn = jeml`{def x, y, z}
+test('{def block}', function (t) {
+    var fn, result, expected
+
+    fn = jeml`{def x, y, z}
         {x} {y} {z}
     `
-    var result = fn(1, 2, 3).replace(/w+/, '')
-    var expected = '123'
-    // assert(result === expected)
+    result = fn(1, 2, 3).replace(/ /g, '')
+    expected = '123'
+    t.equal(result, expected)
 
     // should throw
-    var fn = jeml`{def name}
+    fn = jeml`{def name}
         <p> hello, { name } </p>
     `
+    result = fn('okkk').replace(/ /g, '')
+    expected = '<p>hello,okkk</p>'
+    t.equal(result, expected)
 
-    t.equal(
-        fn('okkk').replace(/ /g, ''),
-        '<p>hello,okkk</p>'
-    )
+    fn = jeml`{def a, b, c, d, e, f}`
+    result = fn.length
+    expected = 6
+    t.equal(result, expected)
 
     t.end()
 })
 
-test('${} block', function (t) {
+test('${ block }', function (t) {
+    var fn, result, expected
 
-    var fn = jeml`
+    fn = jeml`
         <p> ${ 'hello' } </p>
     `
+    result = fn().replace(/ /g, '')
+    expected = '<p>hello</p>'
+    t.equal(result, expected)
 
-    // should escaped
-
-    t.equal(
-        fn().replace(/ /g, ''),
-        '<p>hello</p>'
-    )
+    fn = jeml`
+        ${ "&><'" }
+    `
+    result = fn().replace(/ /g, '')
+    expected = '&amp;&gt;&lt;&#39;'
+    t.equal(result, expected)
 
     t.end()
 })
@@ -56,7 +65,43 @@ test('${} block', function (t) {
 // {else}        // } else {
 // {end}         // => }
 //
-test('if block', function (t) {
+test('{if block}', function (t) {
+    var fn, result, expected
+
+    fn = jeml`
+        {if 10 > 5}
+            okk
+        {end}
+    `
+    result = fn().replace(/ /g, '')
+    expected = 'okk'
+    t.equal(result, expected)
+
+    fn = jeml`
+        {if 10 < 5}
+            none
+        {else}
+            okk
+        {end}
+    `
+    result = fn().replace(/ /g, '')
+    expected = 'okk'
+    t.equal(result, expected)
+
+    fn = jeml`
+        {if false}
+            bad
+        {else if false}
+            bad
+        {else if true}
+            okk
+        {else}
+            bad
+        {end}
+    `
+    result = fn().replace(/ /g, '')
+    expected = 'okk'
+    t.equal(result, expected)
 
     t.end()
 })
@@ -68,44 +113,111 @@ test('if block', function (t) {
 // {each name in abc if name > 10} // for (var i = 0; i < xx; i++) { if(exp) {continue}
 //
 test('each block', function (t) {
+    var fn, result, expected
+
+    fn = jeml`
+        {each name in ${[1, 2, 3, 4]}}
+            <p>{name}</p>
+        {end}
+    `
+    result = fn().replace(/ /g, '')
+    expected = '<p>1</p><p>2</p><p>3</p><p>4</p>'
+    t.equal(result, expected)
+
+    fn = jeml`
+        {each index, value in ${['hello', 'good', 'bye']}}
+            <p>{index} : {value}</p>
+        {end}
+    `
+    result = fn().replace(/ /g, '')
+    expected = '<p>0:hello</p><p>1:good</p><p>2:bye</p>'
+    t.equal(result, expected)
 
     t.end()
 })
 
 // escaped
-test('expression block', function (t) {
+test('{expression}', function (t) {
+    var fn, result, expected
+
+    fn = jeml`{def name}
+        {"hello," + name}
+    `
+    result = fn("ggyy").replace(/ /g, '')
+    expected = 'hello,ggyy'
+    t.equal(result, expected)
+
+    fn = jeml`
+        { "><'"}
+    `
+    result = fn().replace(/ /g, '')
+    expected = '&gt;&lt;&#39;'
+    t.equal(result, expected)
 
     t.end()
 })
 
 // unescaped
 test('raw block', function (t) {
+    var fn, result, expected
+
+    fn = jeml`
+        {raw "><'"}
+    `
+    result = fn().replace(/ /g, '')
+    expected = '><\''
+    t.equal(result, expected)
+
+    var f1 = jeml`{def name}
+        <hello> {name} </hello>
+    `
+    var f2 = jeml`
+        <strong>{raw ${f1}("okk")}</strong>
+    `
+    result = f2().replace(/ /g, '')
+    expected = '<strong><hello>okk</hello></strong>'
+    t.equal(result, expected)
 
     t.end()
 })
 
-//
-// {script $(function (global) { ... })}
+
+// {script ${function (global) { ... }}}
 // =>
-// // bad ? <script> var global = (new Function('return this')()); ... </script> // problematic ?
+// bad ? <script> var global = (new Function('return this')()); ... </script> // problematic ?
 // or ?
-// // perfered <script> ;(function(global) { ... }(new Function('return this')())); <script>
-//
+// perfered <script> ;(function(global) { ... }(new Function('return this')())); <script>
+
 // throw exception if found '</script>' ???
-test('script block', function (t) {
-    var fn = jeml`
-        {script ${`
-            var x = 10;
-            var y = 20;
-        `}}
+test('{script block}', function (t) {
+    var fn, result, expected
+
+    fn = jeml`
+        hello
+        {script ${0,`var x = 10;var y = 20;`}}
     `
+    // console.log(fn.toString())
+    result = fn().replace(/ /g, '')
+    expected = 'hello<script>varx=10;vary=20;</script>'
+    t.equal(result, expected)
+
     t.end()
 })
 
 //
 // {style ${`body { background: #ffffff }`}} // minify ?
 //
-test('style block', function (t) {
+test('{style block}', function (t) {
+    var fn, result, expected
+
+    try {
+    fn = jeml`
+        {style ${0,`body {background: red}`}}
+    `
+    } catch (e) {console.log(e.raw); throw e}
+    result = fn().replace(/ /g, '')
+    expected = '<style>body{background:red}</style>'
+    t.equal(result, expected)
 
     t.end()
 })
