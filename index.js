349e543784240d27c34961ddfbed84360f6c0168
gyson <eilian.yunsong@gmail.com>
2015-06-18 06:41:04 -0400

    refactor jeml implementation

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 var assert = require('assert')
 
-module.exports = exports = jeml.default = jeml
-
-var hasEscapedChars = /[&<>"]/; // need this ?
+module.exports = jeml
 
 var reEscape = /[&<>"'\/]/g
 
@@ -31,16 +29,16 @@ function escape(str) {
     return str.replace(reEscape, fnEscape)
 }
 
-var ESCAPE = '__jeml_escape__' // + Date.now()
-var INPUTS = '__jeml_inputs__' // + Date.now()
-var JEML_STRING = '__jeml_s__'
+var ESCAPE = '_jeml_escape_'
+var INPUTS = '_jeml_inputs_'
+var JEML_STRING = '_jeml_s_'
 
 var reBody = /(\}(?:\\\{|[^\{])*)|(\{(?:\\\}|[^\}])*)/g
 var reInput = new RegExp(`[=?!]?\\(${ INPUTS }\\[(\\d+)\\]\\)`, 'g')
 
 var count = 0
 function getUniqueName() {
-    return `__jeml_${ count++ }__`
+    return `_jeml_${count++}_`
 }
 
 // by default, it will eliminate space...
@@ -50,11 +48,11 @@ function jeml(strings) {
 
     var str  = '}' + strings.raw[0]
     for (var i = 1; i < arguments.length; i++) {
-        str += `(${ INPUTS }[${ i-1 }])`
+        str += ` (${INPUTS}[${i - 1}]) `
         str += strings.raw[i]
     }
 
-    var args = undefined
+    var args = ''
     var body = ''
 
     var result = str.replace(reBody, function (match, text, exp, index, src) {
@@ -64,7 +62,6 @@ function jeml(strings) {
             // {} ${}
             // { exp ? 'selected' : '' }
             // xxx="{expression}" yyy="/name/good/${expression}"
-
             var ret = text.slice(1)
                     .replace(reInput, function (_, index) {
                         return escape(inputs[index])
@@ -72,11 +69,11 @@ function jeml(strings) {
                     .replace(/\s+/g, ' ')
                     .replace(/\\\{/g, '{')
                     .replace(/\\\}/g, '}')
-                    .replace(/\'/g, '\'')
+                    .replace(/\'/g, '\\\'')
             if (ret === '') {
                 return ''
             } else {
-                return `${JEML_STRING} += \'${ret}\';`
+                return `${JEML_STRING}+=\'${ret}\';`
             }
         }
 
@@ -104,7 +101,7 @@ function jeml(strings) {
             var index = parsed[1]
             var value = parsed[2]
             var exp = parsed[3]
-            return `for (var ${i} = 0, ${e} = ${exp}, ${l} = (${e}&&${e}.length) || 0; ${i} < ${l}; ${i}++) { var ${value} = ${e}[i]; var ${index} = ${i};`
+            return `for(var ${i}=0,${e}=${exp},${l}=(${e}&&${e}.length)||0;${i}<${l};${i}++){var ${value}=${e}[${i}],${index}=${i};`
         }
 
         // {each value in exp}
@@ -116,20 +113,20 @@ function jeml(strings) {
             var l = getUniqueName()
             var value = parsed[1]
             var exp = parsed[2]
-            return `for (var ${i} = 0, ${e} = ${exp}, ${l} = (${e}&&${e}.length) || 0; ${i} < ${l}; ${i}++) { var ${value} = ${e}[i];`
+            return `for(var ${i}=0,${e}=${exp},${l}=(${e}&&${e}.length)||0;${i}<${l};${i}++){var ${value}=${e}[${i}];`
         }
 
         // {else if exp}
         // } else if (exp) {
         if (parsed = /\s*else\s+if\s+([^]*)/.exec(ret)) {
-            return `}else if(${ parsed[1]}){`
+            return `}else if(${parsed[1]}){`
         }
 
         // {if exp}
         // if (exp) {
         if (parsed = /\s*if\s+([^]*)/.exec(ret)) {
             // console.log(parsed)
-            return `if(${ parsed[1] }){`
+            return `if(${parsed[1]}){`
         }
 
         // {else}
@@ -149,42 +146,36 @@ function jeml(strings) {
             return `${JEML_STRING}+=${parsed[1]};`
         }
 
-        // {script ${` var a = 10; var b = 20 `}}
-        // {script ${ fs.readFileSync("path/to/file.js") }}
+        // {script ${0, ` var a = 10; var b = 20 `}}
+        // {script ${fs.readFileSync("path/to/file.js")}}
         // check escape ? </script> tag ?
         if (parsed = /\s*script\s+([^]*)/.exec(ret)) {
             // parse
             // assert it's ${ ... }
-            return `${JEML_STRING}+='<script type="text/javascript">';${JEML_STRING}+=${ parsed[1] };${JEML_STRING}+='</script>';`
+            // var script = parsed[1]
+            return `${JEML_STRING}+='<script>';${JEML_STRING}+=${parsed[1]};${JEML_STRING}+='</script>';`
         }
 
-        // {style ${`body { background: }`}}
-        // {style ${ fs.readFileSync("path/to/file.css") }}
+        // {style ${0, `body { background: }`}}
+        // {style ${fs.readFileSync("path/to/file.css")}}
         if (parsed = /style\s+([^]*)/.exec(ret)) {
             // assert it's ${` body ... `}
-            return `${JEML_STRING}+='<style type="text/css">';${JEML_STRING}+=${ parsed[1] };${JEML_STRING}+='</style>';`
+            return `${JEML_STRING}+='<style>';${JEML_STRING}+=${parsed[1]};${JEML_STRING}+='</style>';`
         }
 
         // it's just expression
         return `${JEML_STRING}+=${ESCAPE}((${ret}));`
-
-        // __jeml_0__
-        // __jeml_1__
-        // ...
     })
 
     // process args
     // args = args || 'function(){"use strict";'
 
-    var body = `function(${ args }){'use strict'; var ${JEML_STRING} = ''; ${ result } return ${JEML_STRING};}`
-
-    // var __jeml_0__ = arguments[1][0]
-    // var __jeml_1__ = argumetns[1][1]
+    var body = `function(${args}){'use strict';var ${JEML_STRING}='';${result};return ${JEML_STRING}}`
 
     try {
         return (new Function(ESCAPE, INPUTS, `return ${body}`)(escape, inputs))
     } catch (e) {
-        e.raw = body
+        e.raw = body // or e.body ?
         throw e
     }
 }
