d0d8645904ccfdf40fbe99d577612ab27a7e2692
gyson <eilian.yunsong@gmail.com>
2015-06-23 19:41:24 -0400

    add nice stack trace support

--- index.js
+++ index.js
@@ -31,20 +31,31 @@ function escape(str) {
     return str.replace(reEscape, fnEscape)
 }
 
+function assert(bool, message) {
+    if (!bool) {
+        throw new Error(message || '')
+    }
+}
+
 var ESCAPE = '_jeml_escape_'
 var INPUTS = '_jeml_inputs_'
 var JEML_STRING = '_jeml_s_'
+var JEML_ASSERT = '_jeml_assert_'
 
 var reBody = /(\}(?:\\\{|[^\{])*)|(\{(?:\\\}|[^\}])*)/g
-var reInput = new RegExp(`\\(${INPUTS}\\[(\\d+)\\]\\)`, 'g')
+var reInput = new RegExp(`(\\(${INPUTS}\\[\\d+\\]\\))`)
 
 var count = 0
 function getUniqueName() {
     return `_jeml_${count++}_`
 }
 
+// line number
 function jeml(strings) {
     var inputs = [].slice.call(arguments, 1)
+
+    var stack = new Error().stack.split('\n')[2]
+    var line = ~~(/:(\d+):\d+/.exec(stack)[1]) - 1
 
     var str  = '}' + strings.raw[0]
     for (var i = 1; i < arguments.length; i++) {
@@ -55,30 +66,31 @@ function jeml(strings) {
     var args = ''
     var body = ''
 
-    var result = str.replace(reBody, function (match, text, exp, index, src) {
-        // text
+    var result = str.replace(reBody, function (match, text, exp) {
         if (match[0] === '}') {
-            var ret = text.slice(1)
-                    .replace(reInput, function (_, index) {
-                        return String(inputs[index])
-                    })
-                    .replace(/\s+/g, ' ')
-                    .replace(/\\\{/g, '{')
-                    .replace(/\\\}/g, '}')
-                    .replace(/\'/g, '\\\'')
-            if (ret === '') {
-                return ''
-            } else {
-                return `${JEML_STRING}+=\'${ret}\';`
-            }
+            var result = ''
+            text.slice(1).split(reInput).forEach(function (str, index) {
+                if (index % 2 === 0) {
+                    str = str.replace(/\n/g, '\\\n\\n')
+                            .replace(/\\\{/g, '{')
+                            .replace(/\\\}/g, '}')
+                            .replace(/\'/g, '\\\'')
+                    if (str !== '') {
+                        result += `${JEML_STRING}+=\'${str}\';`
+                    }
+                } else {
+                    result += `${JEML_STRING}+=${str};`
+                }
+            })
+            return result
         }
 
-        // exp
-        // if (match[0] === '{') {
-        var ret = exp.replace(/\{\s*/, '')
+        var ret = exp.slice(1)
+                    .replace(/\\\{/g, '{')
+                    .replace(/\\\}/g, '}')
         var parsed
 
-        // { def a, b, c }
+        // {def a, b, c}
         if (parsed = /^\s*def\s*([^]*)/.exec(ret)) {
             args = parsed[1]
             return ''
@@ -92,7 +104,7 @@ function jeml(strings) {
             var index = parsed[1]
             var value = parsed[2]
             var exp = parsed[3]
-            return `for(var ${i}=0,${e}=${exp},${l}=(${e}&&${e}.length)||0;${i}<${l};${i}++){var ${value}=${e}[${i}],${index}=${i};`
+            return `for(var ${i}=0,${e}=(${exp}),${l}=(${e}&&${e}.length)||0;${i}<${l};${i}++){var ${value}=${e}[${i}],${index}=${i};`
         }
 
         // {each value in exp}
@@ -102,7 +114,13 @@ function jeml(strings) {
             var l = getUniqueName()
             var value = parsed[1]
             var exp = parsed[2]
-            return `for(var ${i}=0,${e}=${exp},${l}=(${e}&&${e}.length)||0;${i}<${l};${i}++){var ${value}=${e}[${i}];`
+            return `for(var ${i}=0,${e}=(${exp}),${l}=(${e}&&${e}.length)||0;${i}<${l};${i}++){var ${value}=${e}[${i}];`
+        }
+
+        // {for value of map}
+        // {for key in obj}
+        if (parsed = /^\s*for\s*([^]*)/.exec(ret)) {
+            return `for(var ${parsed[1]}){`
         }
 
         // {else if exp}
@@ -126,13 +144,13 @@ function jeml(strings) {
             return '}'
         }
 
-        if (parsed = /^\s*raw\s*([^]*)/.exec(ret)) {
-            return `${JEML_STRING}+=${parsed[1]};`
+        // {js throw new Error()}
+        if (parsed = /^\s*js\s*([^]*)/.exec(ret)) {
+            return `${parsed[1]};`
         }
 
-        // alias to raw
-        if (parsed = /^\s*>\s*([^]*)/.exec(ret)) {
-            return `${JEML_STRING}+=${parsed[1]};`
+        if (parsed = /^\s*assert\s*([^]*)/.exec(ret)) {
+            return `${JEML_ASSERT}(${parsed[1]});`
         }
 
         // {script ${fs.readFileSync("path/to/file.js")}}
@@ -145,20 +163,57 @@ function jeml(strings) {
             return `${JEML_STRING}+='<style>';${JEML_STRING}+=${parsed[1]};${JEML_STRING}+='</style>';`
         }
 
+        // unescape
+        if (parsed = /^\s*>\s*([^]*)/.exec(ret)) {
+            return `${JEML_STRING}+=${parsed[1]};`
+        }
+
+        // escape
         if (parsed = /^\s*=\s*([^]*)/.exec(ret)) {
             return `${JEML_STRING}+=${ESCAPE}((${parsed[1]}));`
         }
 
-        // need this ? throw error ?
-        return `${JEML_STRING}+=${ESCAPE}((${ret}));`
+        // comment
+        if (parsed = /^\s*\*\s*([^]*)/.exec(ret)) {
+            return parsed[1].replace(/[^\n]/g, '')
+        }
+
+        // line number ?
+        var err = new Error(`Invalid expression: \`{${ret}}\``)
+        err.body = ''
+        throw err // jeml syntax error
     })
 
-    var body = `function(${args}){'use strict';var ${JEML_STRING}='';${result};return ${JEML_STRING}}`
+    var nameId = getUniqueName() + '_fn_name_'
+    var body = `function ${nameId}(${args}){var ${JEML_STRING}='';${result};return ${JEML_STRING}}`
 
     try {
-        return (new Function(ESCAPE, INPUTS, `return ${body}`)(escape, inputs))
+        var fn = (new Function(ESCAPE, INPUTS, JEML_ASSERT, `'use strict';return ${body}`)(escape, inputs, assert))
+        return (new Function('fn', 'stack', 'lineNum', `
+            function _jeml_ignore_(${args}) {
+                try {
+                    return fn.apply(this, arguments)
+                } catch (e) {
+                    if (e && typeof e.stack === 'string') {
+                        e.stack = e.stack.split('\\n').map(function (line) {
+                            if (/${nameId}/.exec(line)) {
+                                var ret = (/<anonymous>:(\\d+):(\\d+)/.exec(line))
+                                return stack.replace(/:\\d+:\\d+/, ':' + (~~ret[1] + lineNum) + ':' + ret[2])
+                            }
+                            return line
+                        }).filter(function (line) {
+                            return !(/_jeml_ignore_/.exec(line))
+                        }).join('\\n')
+                    }
+                    throw e // jeml runtime error
+                }
+            };
+            _jeml_ignore_.toString = fn.toString.bind(fn);
+            return _jeml_ignore_;
+        `)(fn, stack, line))
     } catch (e) {
-        e.raw = body // or e.body
-        throw e
+        e.message += ` while compiling jeml template`
+        e.body = body
+        throw e // jeml syntax error
     }
 }
